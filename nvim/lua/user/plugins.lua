c77eebaa02ac5a7c405dc4e63c7fc2677153fdb6
Johnathan Barrett <johnathan@complyfirst.co>
2026-03-16 16:30:34 +0000

    feat(telescope): add document symbols keybinding
    Add <leader>r keybinding to open LSP document symbols in Telescope, enabling quick navigation to symbols within the current document.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -279,6 +279,7 @@ return {
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
       vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
+      vim.keymap.set("n", "<leader>r", builtin.lsp_document_symbols, { desc = "Document symbols" })
     end,
   },
 
